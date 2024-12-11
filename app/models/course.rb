2f7387f10d89ddb44252c97b3296d5db868017fa
Matias Bensan Araya <mbensan@gmail.com>
2024-12-10 21:39:23 -0300

    Se alade relacion NaN

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   belongs_to :user
+  has_many :enrollments
+  has_many :users, through: :enrollments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # cursos en los que el usuario es instructor
   has_many :courses
+
+  has_many :enrollments, dependent: :destroy
+  
+  has_many :enrolled_courses, through: :enrollments, source: :course
 end
